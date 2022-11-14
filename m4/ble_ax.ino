f8ee379ec1ef08d52da9cd5e338c3bb146daead6
vanisharmag <67726421+vanisharmag@users.noreply.github.com>
2022-11-14 13:55:14 -0800

    Create ble_ax.ino

---
+++ m4/ble_ax.ino
@@ -0,0 +1,69 @@
+#include <ArduinoBLE.h>
+#include <Arduino_LSM6DS3.h>
+
+#define BLE_UUID_ACCELEROMETER_SERVICE "1101"
+#define BLE_UUID_ACCELEROMETER_X "2101"
+
+#define BLE_DEVICE_NAME "Vani"
+#define BLE_LOCAL_NAME "Vani"
+
+BLEService accelerometerService(BLE_UUID_ACCELEROMETER_SERVICE);
+
+BLEFloatCharacteristic accelerometerCharacteristicX(BLE_UUID_ACCELEROMETER_X, BLERead | BLENotify);
+
+float ax, ay, az;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // initialize IMU
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  // initialize BLE
+  if (!BLE.begin()) {
+    Serial.println("Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+
+  // set advertised local name and service UUID
+  BLE.setDeviceName("Vani : Nano 33 IoT");
+  BLE.setAdvertisedService(accelerometerService);
+
+  // add characteristics and service
+  accelerometerService.addCharacteristic(accelerometerCharacteristicX);
+ 
+  BLE.addService(accelerometerService);
+
+  accelerometerCharacteristicX.writeValue(0.0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE IMU Peripheral");
+}
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) { 
+
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(ax, ay, az);
+        accelerometerCharacteristicX.writeValue(ax);
+      }
+    }
+  }
+}
